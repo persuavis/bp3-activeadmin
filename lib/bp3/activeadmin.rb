b2b661c366927dffe9477fb9781d797141005c83
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 12:26:42 -0800

    Initial commit

---
+++ lib/bp3/activeadmin.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'activeadmin/version'
+
+module Bp3
+  module Activeadmin
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
